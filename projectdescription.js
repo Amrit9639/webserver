702a5ce02eb9c8dd736cd1a518fc05d101e84072
Amritpal Singh <748914@student.tor.loc>
2019-07-31 15:16:25 -0400

    3:15pm commit

--- projectdescription.js
+++ projectdescription.js
@@ -1,7 +1,11 @@
-const http = reqire('http');
-
+const http = require('http');
+const url = require('url');
 function handler(req,res){
+    const parseUrl=url.parse(req.url,true);
+    console.log(parseUrl);
      res.writeHead (200, {'Content-type':'text/plain'});
+     res.write('hello, I am a webserver!')
 }
 const server = http.createServer(handler);
 
+server.listen(3000);
